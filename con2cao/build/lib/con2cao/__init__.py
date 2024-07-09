b56a4c3321199ba67b874bc4b081653941473ea5
gw3746 <77274059+gw3746@users.noreply.github.com>
2024-07-09 21:19:09 +0800

    commit 202407090918

--- con2cao/build/lib/con2cao/__init__.py
+++ con2cao/build/lib/con2cao/__init__.py
@@ -1,3 +1,3 @@
-from .con2fuxi import Con2fuxi
-from .con2fuxi import Con2cao
-from .con2fuxi import convert_pg_type_to_mysql_type
+from .con2cao import Con2fuxi
+from .con2cao import Con2cao
+from .con2cao import convert_pg_type_to_mysql_type
