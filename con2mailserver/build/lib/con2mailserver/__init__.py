d8e0f586d6c11c00aef0207ec428b72d938b0a05
gw3746 <77274059+gw3746@users.noreply.github.com>
2024-08-20 21:35:44 +0800

    update con2mailserver

--- con2mailserver/build/lib/con2mailserver/__init__.py
+++ con2mailserver/build/lib/con2mailserver/__init__.py
@@ -1,2 +1,2 @@
 # con2mailserver/__init__.py
-from .con2mailserver import con2gmailserver, con2hinetserver, con2mailserver
+from .con2mailserver import con2gmailserver, con2hinetserver, con2mailserver,test_conect_test
